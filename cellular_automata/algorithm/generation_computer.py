9a1a96fd768cef15b59d93082cd5e669fddef3f5
Surender Harsha <battusurender96@gmail.com>
2025-09-06 23:25:59 +0200

    added file structure and initial unit test

---
+++ cellular_automata/algorithm/generation_computer.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class GenCompute:
+    def __init__(self):
+
+        pass
+
+    def next_step(self, grid: np.array, kernel: np.array, birth: list, survival: list):
+
+        # Loop the kernel over the grid, to compute neighbours for each cell. Use wrap around.
+
+        # Keep the neighbours values in a separate array
+
+        # For cells with 0 in the grid, check for birth conditions using neighbour array
+
+        # For cells with 1 in the grid, check for survival conditions using neighbour array
+
+        # Create new generation of grid
+
+        # return grid, please.
+        pass
+
+
+if __name__ == '__main__':
+    a = GenCompute()
---
+++ tests/gen_step_test.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from cellular_automata.algorithm.generation_computer import GenCompute
+
+
+def test_first_gen():
+
+    grid = np.zeros(shape=(10, 10))
+    grid[4][4] = 1
+    grid[4][5] = 1
+    grid[5][4] = 1
+    grid[5][5] = 1
+
+    kernel = np.zeros(shape=(3, 3))
+    kernel[0][0] = 1
+    kernel[0][1] = 1
+    kernel[-1][0] = 1
+    kernel[-1][1] = 1
+
+    birth = [2, 7]
+    survival = [2, 7]
+
+    test_object = GenCompute()
+    new_grid = test_object.next_step(grid, kernel, birth, survival)
+
+    target_grid = np.zeros(shape=(10, 10))
+    target_grid[3][5] = 1
+    target_grid[4][5] = 1
+    target_grid[5][5] = 1
+    target_grid[6][5] = 1
+
+    np.testing.assert_equal(new_grid, target_grid)
+
+
