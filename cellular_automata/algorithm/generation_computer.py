a83493c7022123ae6426a78f927f3a7e56854cf9
ElisaSofiaValerio <valerio.elisasofia98@gmail.com>
2025-09-12 00:18:37 +0200

    First attempt at the next_step function. Correl2D. Mask and inverted mask. Test succeeded.

--- cellular_automata/algorithm/generation_computer.py
+++ cellular_automata/algorithm/generation_computer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.signal
 
 
 class GenCompute:
@@ -9,17 +10,33 @@ class GenCompute:
     def next_step(self, grid: np.array, kernel: np.array, birth: list, survival: list):
 
         # Loop the kernel over the grid, to compute neighbours for each cell. Use wrap around.
-
         # Keep the neighbours values in a separate array
+        neighbours_grid = scipy.signal.correlate2d(grid, kernel, mode='same', boundary='wrap')
+
 
         # For cells with 0 in the grid, check for birth conditions using neighbour array
+        next_gen_grid = grid
+        mask_grid = grid * neighbours_grid
+        index_r, index_c = np.where(mask_grid != 0)
+        for i in range(len(index_r)):
+            if neighbours_grid[index_r[i]][index_c[i]] in birth:
+                next_gen_grid[index_r[i]][index_c[i]] = 1
+            else:
+                next_gen_grid[index_r[i]][index_c[i]] = 0
 
         # For cells with 1 in the grid, check for survival conditions using neighbour array
+        mask_grid = (1 - grid) * neighbours_grid
+        index_r, index_c = np.where(mask_grid != 0)
+        for i in range(len(index_r)):
+            if neighbours_grid[index_r[i]][index_c[i]] in survival:
+                next_gen_grid[index_r[i]][index_c[i]] = 1
+            else:
+                next_gen_grid[index_r[i]][index_c[i]] = 0
 
         # Create new generation of grid
 
         # return grid, please.
-        return grid
+        return next_gen_grid
 
 
 if __name__ == '__main__':
